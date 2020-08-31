d9e3a37230ff5b52c62fcf8f18d78f85fcde888f
Lukas Jurk <lukas.jurk@tu-dresden.de>
2020-08-31 10:15:57 +0200

    parse connections

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
 
@@ -6,42 +6,56 @@ app = Flask(__name__)
 api = Api(app)
 
 
+def parseSections(data):
+    clientData = {}
+    for section in data:
+        name = ""
+        port = ""
+        connections = []
+        for client in section.split('\n'):
+            if client.startswith('\tproperties'):
+                #properties
+                prop = client.strip().replace("properties: ","").split(',')[0]
+                if prop not in clientData[name]:
+                    clientData[name][prop] = {}
+                continue
+            if client.startswith('   '):
+                #connections
+                connections.append(client.strip())
+                print("connection")
+                continue
+            name = client.split(':')[0]
+            port = client.split(':')[1]
+            if name not in clientData:
+                clientData[name] = {}
+        clientData[name][prop][port] = {}
+        clientData[name][prop][port]['connections'] = connections
+
+    return clientData
 
 def getClients():
     try:
-        clients = check_output(['jack_lsp', '-p']).decode('utf-8')
-    except subprocess.CalledProcessError:
+        clients = check_output(['jack_lsp', '-c', '-p']).decode('utf-8')
+    except CalledProcessError:
         return []
     if not clients:
         return []
 
-    clientData = {}
 
-    clients = clients.replace('\n\tproperties', '')
-    for client in clients.split('\n'):
-        data = client.split(':')
-        if len(data) < 2:
-            continue
-        name = data[0]
-        port = data[1]
-        properties = data[2].strip().split(',')[0]
-        if name not in clientData:
-            clientData[name] = {}
-        if properties not in clientData[name]:
-            clientData[name][properties] = []
-        clientData[name][properties].append(port)
+    # split output into sections
+    sections = clients.strip().split(',\n')
 
-    return clientData
+    return parseSections(sections)
+
 
 class Clients(Resource):
     def get(self):
-        return self.getClients()
+        return getClients()
 
+api.add_resource(Clients, '/')
 
 @app.route('/ui', methods=['GET', 'POST'])
 def getIndex():
-    print(getClients())
     return render_template('index.html', clients=getClients())
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="2323", debug=True)
+if __name__ == '__main__':    app.run(host='0.0.0.0', port="2323", debug=True)
