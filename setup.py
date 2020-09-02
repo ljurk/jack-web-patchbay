300751ffc1e544e06ea69322d937fe79a4e66e58
Lukas Jurk <lukas.jurk@tu-dresden.de>
2020-09-02 16:53:29 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jack_web',
-    version='0.0.1',
+    version='0.0.2',
     packages=['jack_web'],
     author="Lukas Jurk",
     author_email="ljurk@pm.me",
